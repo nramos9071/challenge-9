d3dc7464af9f092f1c33fbb2f2c4dedc7cf6e7f7
nramos9071 <nramos9071@gmail.com>
2024-10-10 19:51:07 -0500

    updates

--- index.js
+++ index.js
@@ -1,76 +1,98 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
-const  generateMarkdown  = require('./generateMarkdown');
 
-
-
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         message: 'what is your GitHub username?',
-        name: 'user-name',
+        name: 'userName',
     },
+
     {
         type: 'input',
         message: 'what is your email?',
         name: 'email',
     },
-    
+
     {
         type: 'input',
         message: 'What is the Title of your project?',
         name: 'title',
 
     },
+
     {
         type: 'input',
         message:'Please describe your project.',
         name: 'description',
     },
+
+    {
+        type: 'input',
+        message: 'How do you install it?',
+        name: 'installation'
+    },
+
+    {
+        type: 'input',
+        message: 'How do you use your project?',
+        name: 'usage',
+    },
+    
     {
         type: 'input',
         message: 'Which license are you using?',
         name: 'license'
     }
 
-  
 ];
 
-
-// TODO: Create a function to write README file
-function writeToFile() {
-
-    fs.writeFile('README.md', JSON.stringify(response), (err) => {
-    err ? console.error(err) : console.log('README Created!')
-    })
-
-};
 
 function init() {
 
     
 
-inquirer
-    .prompt(questions)
-    .then(response => {
+inquirer.prompt(questions).then(answers => {
+    const readmeContent = `
+# ${answers.title}
 
-        fs.writeFile('README.md', JSON.stringify(response), (err) => {
-            err ? console.error(err) : console.log('README Created!')
-            })
-    })
-    // .then(generateMarkdown)
+##Table of Contents
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[Contributing](#contributing)
+[License](#license)
 
-        
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+
+## Contributing
+${answers.userName}
+${answers.email}
+
+## License
+${answers.license} `;
+
+   fs.writeFile('README.md', readmeContent, (err) => {
+       if (err) throw err;
+       console.log('README.md has been generated!');
+   });
+});
+}
+       
 
       
 
-// TODO: Create a function to initialize app
-
-};
-// Function call to initialize app
 init();
+
+
 
 
 
